94624369360192a5b5fb9cb5444374afdc1d7827
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2015-09-01 19:28:26 -0300

    gulp modules & needed dependencies

---
+++ app/templates/gulp/_gulpfile.js
@@ -0,0 +1,3 @@
+var gulp          = require('gulp');
+
+require('require-dir')('assets/src/gulp');
---
+++ app/templates/gulp/modules/static.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+var gulp = require('gulp');
+var del  = require('del');
+
+
+
+gulp.task('clean', function(cb) {
+  del([paths.dist], cb);
+});
