0a89206ef68f04f66cc7467fd9c7955e7f7b05d3
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2015-09-17 18:16:24 -0300

    basscss on sass: Closes #22

--- app/templates/gulp/modules/vendor/basscss.js
+++ app/templates/gulp/modules/vendor/basscss.js
@@ -2,11 +2,20 @@
 
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+
+<% if (cssProcessor === 'scss') { %>
+var sass = require('gulp-ruby-sass');
+<% } %>
+<% if (cssProcessor === 'less') { %>
+var less = require('gulp-less');
+<% } %>
+<% if (cssProcessor === 'styl') { %>
+var stylus = require('gulp-stylus');
+<% } %>
+
 var minify = require('gulp-minify-css');
 var plumber = require('gulp-plumber');
 var autoprefixer  = require('gulp-autoprefixer');
-
 
 var rename = require('gulp-rename');
 
@@ -15,13 +24,54 @@ var onError = function(err) {
 }
 
 
-gulp.task('vendor:basscss', function() {
-  return gulp.src('assets/src/vendor/basscss-sass/basscss.scss')
+gulp.task('vendor:basscss:styles', function() {
+
+  return gulp.src('assets/src/<%= cssProcessor %>/vendor/basscss/index.<%= cssProcessor %>')
+
     .pipe(plumber({ errorHandler: onError }))
-    .pipe(sass())
+    <% if (cssProcessor === 'scss') { %>
+      .pipe(sass({
+        style: 'compressed',
+        loadPath: ['<%= paths.src.vendors %>/basscss-sass']
+      }))
+    <% } %>
+    <% if (cssProcessor === 'less') { %>
+    .pipe(less())
+    .pipe(rename('basscss.css'))
     .pipe(gulp.dest('assets/dist/css'))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(minify())
+    .pipe(minify({
+      keepSpecialComments: 0
+    }))
     .pipe(rename({suffix: '.min'}))
+    <% } %>
+    <% if (cssProcessor === 'styl') { %>
+    .pipe(stylus())
+    <% } %>
+
+
     .pipe(gulp.dest('assets/dist/css'));
 });
+
+
+
+gulp.task('vendor:basscss:fonts', function() {
+  //TODO
+});
+
+gulp.task('vendor:basscss:scripts', function() {
+  //TODO
+});
+
+gulp.task('vendor:basscss',
+    [ 'vendor:basscss:styles',
+      'vendor:basscss:scripts',
+      'vendor:basscss:fonts' ]
+)
+
+
+
+
+
+
+
