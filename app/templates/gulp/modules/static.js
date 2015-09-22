463af2be621c2ba0f110101e3e8dd48759360c16
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2015-09-22 19:02:40 -0300

    fix static shit

--- app/templates/gulp/modules/static.js
+++ app/templates/gulp/modules/static.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 var gulp = require('gulp');
 var del = require('del');
 var imagemin = require('gulp-imagemin');
@@ -9,7 +8,7 @@ gulp.task('clean', function(cb) {
   del(['<%= paths.dist.base %>'], cb);
 });
 
-gulp.task('static:images', function() {
+gulp.task('images:main', function() {
   return gulp.src('<%= paths.src.images %>/**/*')
     .pipe(imagemin({
       optimizationLevel: 5
@@ -17,10 +16,14 @@ gulp.task('static:images', function() {
     .pipe(gulp.dest('<%= paths.dist.images %>'));
 });
 
-gulp.task('static:fonts', function() {
+gulp.task('fonts:main', function() {
   return gulp.src('<%= paths.src.fonts %>/**/*')
     .pipe(gulp.dest('<%= paths.dist.fonts %>'));
 });
 
+gulp.task('icons:main', function() {
+  return gulp.src('<%= paths.src.icons %>/**/*')
+    .pipe(gulp.dest('<%= paths.dist.icons %>'));
+});
 
-gulp.task('static', ['static:images', 'static:fonts']);
+gulp.task('static', ['images:main', 'fonts:main']);
