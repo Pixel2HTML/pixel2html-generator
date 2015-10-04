ba41f1f630372b9b75faac6e8c64fac953dedc32
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2015-10-03 21:02:37 -0300

    foundation up& running

--- app/templates/gulp/modules/vendor/foundation.js
+++ app/templates/gulp/modules/vendor/foundation.js
@@ -5,9 +5,14 @@ var gulp = require('gulp');
 
 var sass = require('gulp-sass');
 
+var sourcemaps = require('gulp-sourcemaps');
+
 var minify = require('gulp-minify-css');
 var plumber = require('gulp-plumber');
 var autoprefixer = require('gulp-autoprefixer');
+
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 
 var rename = require('gulp-rename');
@@ -22,16 +27,16 @@ gulp.task('vendor:foundation:styles', function() {
   return gulp.src('<%= paths.src.frontendframework %>/foundation/index.scss')
 
   .pipe(plumber({
-    errorHandler: onError
-  }))
-
-  .pipe(sass({
-    style: 'compressed',
-    loadPath: ['<%= paths.src.vendors %>/foundation/scss/foundation']
-  }))
-
-
+      errorHandler: onError
+    }))
     .pipe(rename('foundation.css'))
+    .pipe(sass({
+      outputStyle: 'expanded',
+      includePaths: ['<%= paths.src.vendors %>/foundation/scss/foundation']
+    }))
+
+
+
   .pipe(gulp.dest('<%= paths.dist.styles %>'))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(minify({
@@ -45,16 +50,45 @@ gulp.task('vendor:foundation:styles', function() {
 });
 
 
-
-gulp.task('vendor:foundation:fonts', function() {
-  //TODO
-});
-
 gulp.task('vendor:foundation:scripts', function() {
-  //TODO
+
+  var foundationJsModules = [
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.abide.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.accordion.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.alert.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.clearing.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.dropdown.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.equalizer.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.interchange.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.joyride.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.magellan.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.offcanvas.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.orbit.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.reveal.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.slider.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.tab.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.tooltip.js',
+    '<%= paths.src.vendors %>/foundation/js/foundation/foundation.topbar.js',
+  ];
+
+
+  return gulp.src(foundationJsModules)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat('foundation.js'))
+    .pipe(gulp.dest('<%= paths.dist.scripts %>'))
+    .pipe(uglify({
+      mangle: false
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write())
+
+  .pipe(gulp.dest('<%= paths.dist.scripts %>'));
+
 });
 
 gulp.task('vendor:foundation', ['vendor:foundation:styles',
-  'vendor:foundation:scripts',
-  'vendor:foundation:fonts'
+  'vendor:foundation:scripts'
 ])
