e61d9b17040bc252ffa711948c1817a9b7de516c
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2015-10-07 14:21:36 -0300

    base tests

---
+++ test/general.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var assert = require('yeoman-assert');
+var fs = require('fs');
+
+describe('general', function() {
+
+  before('crafting project', function(done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .inDir(path.join(__dirname, 'temp'))
+      .withOptions({
+        'skip-install': true
+      })
+      .withPrompts({
+        projectName: 'test-project',
+        qtyScreens: 6,
+        projectType: 'responsive',
+        cssProcessor: 'less',
+      })
+      .on('end', done);
+  });
+
+  it('creates expected base files', function() {
+    assert.file([
+      '.bowerrc',
+      '.gitignore',
+      '.gitattributes',
+      '.jshintrc',
+      'bower.json',
+      'package.json',
+      'gulpfile.js',
+      'package.json',
+      '.editorconfig',
+      'screen_1.html',
+      'screen_2.html',
+      'screen_3.html',
+      'screen_4.html',
+      'screen_5.html',
+      'screen_6.html',
+      'assets/src',
+      'assets/src/.gulp',
+      'assets/src/fonts',
+      'assets/src/icons',
+      'assets/src/images',
+      'assets/src/js',
+      'assets/src/styles',
+      'assets/src/vendor',
+    ]);
+  });
+
+  it('should have the project name on package.json', function() {
+    assert.fileContent('package.json',  /"name": "pixel2html-test-project"/);
+  });
+
+
+});
