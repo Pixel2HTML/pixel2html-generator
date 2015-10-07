74739de4706f1991e278054b6943daea297098ac
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2015-10-07 17:17:00 -0300

    clean deps

--- test/less.js
+++ test/less.js
@@ -3,8 +3,6 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 var assert = require('yeoman-assert');
-var fs = require('fs');
-
 
 describe('LESS features', function() {
 
@@ -18,7 +16,7 @@ describe('LESS features', function() {
         projectName: 'test-project',
         qtyScreens: 3,
         projectType: 'responsive',
-        cssProcessor: 'less',
+        cssProcessor: 'less'
       })
       .on('end', done);
   });
