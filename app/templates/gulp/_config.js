5ed119461720f6f054656f3d794ec80adf983cef
Juan Manuel Garcia Olivares <jmgolivares@gmail.com>
2016-11-10 22:56:54 -0300

    vendor scripts

--- app/templates/gulp/_config.js
+++ app/templates/gulp/_config.js
@@ -8,10 +8,10 @@ module.exports = {
       'src/assets/vendor/bootstrap-sass/assets/stylesheets',
       <% } %>
     ],
-    scriptsFiles: [
-      <% if(frontEndFramework == 'bootstrap'){ %>
-      'src/assets/vendor/bootstrap-sass/assets/javascripts/bootstrap.js',
-      <% } %>
+    scriptFiles: [
+      <% if(jQuery){ %>'<%= paths.src.vendors %>/jquery/dist/jquery.js',<% } %>
+      <% if(frontEndFramework == 'bootstrap'){ %>'<%= paths.src.vendors %>/bootstrap-sass/assets/javascripts/bootstrap.js',<% } %>
+      <% if(frontEndFramework == 'foundation'){ %>'<%= paths.src.vendors %>/foundation-sites/dist/foundation.js',<% } %>
     ],
     fontsFiles: [
       <% if(frontEndFramework == 'bootstrap'){ %>
--- app/templates/gulp/tasks/scripts.js
+++ app/templates/gulp/tasks/scripts.js
@@ -8,6 +8,7 @@ var helpers = require('../helpers');
 var concat  = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var uglify  = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 var browserSync = require('browser-sync');
 
@@ -19,6 +20,20 @@ gulp.task('main:scripts', function() {
     .pipe(gulp.dest('<%= paths.dist.scripts %>'))
     .pipe(concat('main.min.js'))
     .pipe(uglify())
+    .pipe(gulp.dest('<%= paths.dist.scripts %>'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('vendor:scripts', function() {
+  return gulp.src(config.vendor.scriptFiles)
+    .pipe(plumber({
+      errorHandler: helpers.onError
+    }))
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('<%= paths.dist.scripts %>'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(gulp.dest('<%= paths.dist.scripts %>'))
     .pipe(browserSync.reload({stream:true}));
 });
