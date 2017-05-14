8f7d9b71673ee60d74b754f469ff305bc5705800
Miguel Palau <mpalauzarza@gmail.com>
2017-05-14 09:08:43 -0500

    remove vendor fonts keep all in 1 fonts task

--- app/templates/gulp/tasks/fonts.js
+++ app/templates/gulp/tasks/fonts.js
@@ -6,8 +6,3 @@ gulp.task('main:fonts', function () {
   return gulp.src(config.project.fontFiles)
     .pipe(gulp.dest(config.directories.dist.fonts))
 })
-
-gulp.task('vendor:fonts', function () {
-  return gulp.src(config.vendor.fontFiles)
-    .pipe(gulp.dest(config.directories.dist.fonts))
-})
