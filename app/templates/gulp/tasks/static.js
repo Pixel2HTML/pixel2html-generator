84562f3736184c9a3c1a6018157a1ab7bbfa3e14
Miguel Palau <mpalauzarza@gmail.com>
2017-09-30 16:27:32 -0500

    static changes

--- app/templates/gulp/tasks/static.js
+++ app/templates/gulp/tasks/static.js
@@ -1,36 +1,12 @@
-var gulp = require('gulp')
-var config = require('../config')
-var $ = require('gulp-load-plugins')()
-
-var del = require('del')
+const gulp = require('gulp')
+const config = require('../config')
+const del = require('del')
 
 gulp.task('clean', function () {
   return del([config.directories.dist.base])
 })
 
-gulp.task('main:images', function () {
+gulp.task('images', function () {
   return gulp.src(config.directories.src.images + '/**/*')
     .pipe(gulp.dest(config.directories.dist.images))
 })
-
-gulp.task('main:icons', function () {
-  return gulp.src(config.directories.src.icons + '/**/*.svg')
-    .pipe($.svgmin({
-      plugins: [
-        {
-          removeStyleElement: true
-        },
-        {
-          removeAttrs: {
-            attrs: ['fill', 'stroke', 'fill.*', 'stroke.*']
-          }
-        }
-      ],
-      js2svg: { pretty: true }
-    }))
-    .pipe($.svgstore())
-    .pipe($.rename('symbols.svg'))
-    .pipe(gulp.dest(config.directories.dist.icons))
-})
-
-gulp.task('main:static', gulp.series('main:images', 'main:icons'))
