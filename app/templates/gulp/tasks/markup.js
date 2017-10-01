7fcefd53038c1ea3b1dca8ff9bdcd1f783c25210
Miguel Palau <mpalauzarza@gmail.com>
2017-09-30 20:26:00 -0500

    adding pug changes

--- app/templates/gulp/tasks/markup.js
+++ app/templates/gulp/tasks/markup.js
@@ -2,33 +2,19 @@ const gulp    = require('gulp')
 const config  = require('../config')
 const $ = require('gulp-load-plugins')()
 const when = require('gulp-if')
+const fs = require('fs')
 const production = config.production
-
-<% if (markupLanguage === 'pug') { -%>
-// Hey don't touch these unless you know what you're doing
-const devLocals = {
-  base: '',
-  extension: '',
-  productionMode: false
-}
-const prodLocals = {
-  base: '/<%= clientId %>/<%= projectId %>',
-  extension: '.html',
-  productionMode: true
-}
-<% } -%>
 
 gulp.task('main:markup', function() {
   return gulp.src(config.directories.src.markup+'/*.<%=markupLanguage%>')
 <% if (markupLanguage === 'pug') { -%>
-    .pipe(when(!production, $.pug({
+    .pipe($.pug({
       baseDir: config.directories.src.markup,
-      locals: devLocals
-    }))).on('error', config.onError)
-    .pipe(when(production, $.pug({
-      baseDir: config.directories.src.markup,
-      locals: prodLocals
-    }))).on('error', config.onError)
+      locals: {
+        icon: name => fs.readFileSync(`./src/assets/icons/${name}.svg`)},
+        production
+      }
+    })).on('error', config.onError)
     .pipe($.htmlPrettify())
 <% } -%>
     .pipe(gulp.dest(config.directories.dist.markup))
