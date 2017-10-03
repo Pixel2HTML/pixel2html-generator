96430ae563bcba0d4bbdeb6da61245001e2ba7ff
Miguel Palau <mpalauzarza@gmail.com>
2017-10-02 22:10:29 -0500

    make html files environment aware

--- app/templates/gulp/tasks/markup.js
+++ app/templates/gulp/tasks/markup.js
@@ -1,9 +1,27 @@
 const gulp    = require('gulp')
 const config  = require('../config')
-<% if (markupLanguage === 'pug') { -%>
 const $ = require('gulp-load-plugins')()
-const fs = require('fs')
 const production = config.production
+<% if (markupLanguage === 'pug') { -%>
+const fs = require('fs')
+<% } -%>
+
+<% if (markupLanguage === 'html') { -%>
+const JS_DIR = config.directories.dist.scripts
+const CSS_DIR = config.directories.dist.styles
+
+const JS_EXT = production ? '.min.js' : '.js'
+const CSS_EXT = production ? '.min.css' : '.css'
+
+const js = [
+  `${JS_DIR}/vendor${JS_EXT}`,
+  `${JS_DIR}/main${JS_EXT}`
+]
+
+const css = [
+  `${CSS_DIR}/vendor${CSS_EXT}`,
+  `${CSS_DIR}/main${CSS_EXT}`
+]
 <% } -%>
 
 gulp.task('markup', function() {
@@ -17,6 +35,9 @@ gulp.task('markup', function() {
       }
     })).on('error', config.onError)
     .pipe($.htmlPrettify())
+<% } -%>
+<% if (markupLanguage === 'html') { -%>
+    .pipe($.htmlReplace({ js, css }))
 <% } -%>
     .pipe(gulp.dest(config.directories.dist.markup))
 })
