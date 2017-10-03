d77d7fe0a19a53d13c783a10ee7259a35113d54f
Miguel Palau <mpalauzarza@gmail.com>
2017-10-02 21:37:27 -0500

    making jekyll environment aware

--- app/templates/gulp/tasks/jekyll.js
+++ app/templates/gulp/tasks/jekyll.js
@@ -1,8 +1,17 @@
 const gulp = require('gulp')
-const spawn = require('child_process').spawn
+const { spawn } = require('child_process')
+const config = require('../config')
+
+// Epic hax to have Jekyll read from the ENV
+// thanks node docs!
+const { env } = require('process')
+env.JEKYLL_ENV = config.production ? 'production' : 'development'
 
 gulp.task('jekyll', function (gulpCallBack) {
-  var jekyll = spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
+  var jekyll = spawn('bundle', ['exec', 'jekyll', 'build'], {
+    stdio: 'inherit',
+    env
+  })
   jekyll.on('exit', function (code) {
     gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code)
   })
